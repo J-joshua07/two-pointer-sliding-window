264d471f5d2f64af10159842baedca02db6eeeb0
J-joshua07 <michaeljoshua638@gmail.com>
2026-08-24 12:50:03 +0530

    In hash map two sum eg added

---
+++ Hashing/hash map/3.py
@@ -0,0 +1,16 @@
+#two sum
+
+arr = [2, 7, 11, 15]
+target = 9
+
+seen = {}
+
+for i in range(len(arr)):
+
+    needed = target - arr[i]
+
+    if needed in seen:
+        print(seen[needed], i)
+        break
+
+    seen[arr[i]] = i
