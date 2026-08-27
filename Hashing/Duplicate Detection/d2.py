7af5895cd9294a1ebb42b4349c5b38a45e0e9865
J-joshua07 <michaeljoshua638@gmail.com>
2026-08-27 12:16:53 +0530

    Duplicate detection eg 2 added

---
+++ Hashing/Duplicate Detection/d2.py
@@ -0,0 +1,15 @@
+#Find ALL Duplicates
+
+arr = [1, 2, 3, 2, 4, 1, 5, 3]
+
+seen = set()
+duplicates = set()
+
+for num in arr:
+
+    if num in seen:
+        duplicates.add(num)
+    else:
+        seen.add(num)
+
+print(duplicates)
