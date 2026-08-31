79c2ad12db416743ddfb61cdd0a6e9068594a93a
J-joshua07 <michaeljoshua638@gmail.com>
2026-08-31 11:10:45 +0530

    Stack valid paranthesis eg  added

---
+++ Stack/valid parathesis/v1.py
@@ -0,0 +1,31 @@
+# valid parathesis
+
+
+def is_valid(s):
+
+    stack = []
+
+    pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for char in s:
+
+        if char in '([{':
+            stack.append(char)
+
+        else:
+            if not stack:
+                return False
+
+            if stack[-1] != pairs[char]:
+                return False
+
+            stack.pop()
+
+    return len(stack) == 0
+
+
+print(is_valid("()"))
