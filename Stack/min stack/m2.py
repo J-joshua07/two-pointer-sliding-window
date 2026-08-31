7ab7b7302ccfdf9069b40e195036a1d46c7b532a
J-joshua07 <michaeljoshua638@gmail.com>
2026-08-31 14:11:09 +0530

    min stack eg 2 added

---
+++ Stack/min stack/m2.py
@@ -0,0 +1,35 @@
+#Simple Min Stack
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+        if not self.min_stack or value <= self.min_stack[-1]:
+            self.min_stack.append(value)
+
+    def pop(self):
+        value = self.stack.pop()
+
+        if value == self.min_stack[-1]:
+            self.min_stack.pop()
+
+        return value
+
+    def get_min(self):
+        return self.min_stack[-1]
+
+
+s = MinStack()
+
+s.push(10)
+s.push(5)
+s.push(8)
+s.push(2)
+s.push(7)
+
+print("Minimum:", s.get_min())
